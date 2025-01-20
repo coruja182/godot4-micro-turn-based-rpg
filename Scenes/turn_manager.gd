ee181cc2ce882ac9d404ba83c4f3f61e7148d020
Luis Henrique Rocha <henrique.srocha@gmail.com>
2025-01-21 00:25:54 +0100

    Turn Manager

---
+++ Scenes/turn_manager.gd
@@ -0,0 +1,47 @@
+class_name TurnManager extends Node
+
+
+@export var characters : Array[Character]
+@export var next_turn_delay_secs : float = 1.0
+@onready var turn_label: Label = $TurnLabel
+
+
+var _current_character : Character
+var game_over : bool = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	assert(len(characters) >= 2, 'must have at least 2 characters')
+	begin_next_turn()
+
+
+func begin_next_turn() -> void:
+	var i_current_char : int = characters.find(_current_character)
+	var next_character_index : int = i_current_char + 1
+	if next_character_index >= len(characters):
+		next_character_index = 0
+	
+	_current_character = characters[next_character_index]
+	update_debug()
+
+
+func end_current_turn() -> void:
+	
+	await get_tree().create_timer(next_turn_delay_secs).timeout
+	
+	if not game_over:
+		begin_next_turn()
+
+
+func character_died(character: Character):
+	game_over = true
+	
+	if character.is_player:
+		print("Game Over!")
+	else:
+		print("You Win!")
+
+
+func update_debug() -> void:
+	turn_label.text = "Turn of %s"  % _current_character.name
